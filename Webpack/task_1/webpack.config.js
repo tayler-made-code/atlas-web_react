4a2ac0621faccce2fe2fd95d11fca49d06760fda
F-O-P <tayler.coon@gmail.com>
2024-05-02 13:07:45 -0500

    fixed typo for entry in webpack.config.js

--- Webpack/task_1/webpack.config.js
+++ Webpack/task_1/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './js/dashboard_mainh.js',
+  entry: './js/dashboard_main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
