d01b4f1f4fd260ed044672a13828d9e7bad6b706
F-O-P <tayler.coon@gmail.com>
2024-05-02 21:37:46 -0500

    added file-loader and image-webpack-loader to webpack.config.js

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -19,6 +19,22 @@ module.exports = {
         generator: {
           filename: 'images/[hash][ext][query]',
         },
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
       },
     ],
   },
