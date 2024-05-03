42e05286626ec6dfa0b6f978b106e95c2b5bc98d
F-O-P <tayler.coon@gmail.com>
2024-05-02 21:17:54 -0500

    setting up task_2, first attempt

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][query]',
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new ImageMinimizerPlugin ({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ['optipng', { optimizationLevel: 5 }],
+              ['jpegtran', { progressive: true }],
+              ['svgo', { removeViewBox: false }],
+            ],
+          },
+        },
+      }),
+    ],
+  },
+};
