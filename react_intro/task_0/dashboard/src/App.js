cf4d568718dc458ffe70161bf465b308384cbfb5
F-O-P <tayler.coon@gmail.com>
2024-05-05 17:51:06 -0500

    task 0 functional

--- react_intro/task_0/dashboard/src/App.js
+++ react_intro/task_0/dashboard/src/App.js
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
+import logo from './holberton_logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="./holberton_logo.jpg" className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
       </header>
+      <hr></hr>
       <body className="App-body">
         <p>Login to access the full dashboard</p>
       </body>
+      <hr></hr>
       <footer className="App-footer">
         <p>Copyright 2024 - holberton School</p>
       </footer>
