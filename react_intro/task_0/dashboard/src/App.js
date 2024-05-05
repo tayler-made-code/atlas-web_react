d686594a4de686cd5cad9cbfaba053468c3a17fa
F-O-P <tayler.coon@gmail.com>
2024-05-05 17:05:29 -0500

    setting up task 0, first attempt

---
+++ react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="./holberton_logo.jpg" className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright 2024 - holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
