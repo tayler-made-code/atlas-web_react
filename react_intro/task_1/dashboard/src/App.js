97e199e4d1d1ae331d31bdec6692b138915726ba
F-O-P <tayler.coon@gmail.com>
2024-05-06 09:10:11 -0500

    task 1 is full functional

---
+++ react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,24 @@
+import logo from './holberton_logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <hr></hr>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <hr></hr>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
