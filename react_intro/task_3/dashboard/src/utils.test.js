4057b46ecf41f43b7d400bb192ba66c232ef42ce
F-O-P <tayler.coon@gmail.com>
2024-05-06 10:35:49 -0500

    added tests for getFullYear, getFooterCopy, and getLatestNotifications

---
+++ react_intro/task_3/dashboard/src/utils.test.js
@@ -0,0 +1,25 @@
+import {getFullYear, getFooterCopy, getLatestNotifications } from './utils';
+
+describe('getFullYear', () => {
+  it('should return the correct year', () => {
+    const currentYear = new Date().getFullYear();
+    expect(getFullYear()).toBe(currentYear);
+  });
+});
+
+describe('getFooterCopy', () => {
+  it('should return "Holberton School" when isIndex is true', () => {
+    expect(getFooterCopy(true)).toBe('Holberton School');
+  });
+
+  it('should return "Holberton School main dashboard" when isIndex is false', () => {
+    expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
+  });
+});
+
+describe('getLatestNotifications', () => {
+  it('should return the correct notification string', () => {
+    const expectedNotification = '<strong>Urgent requirement</strong> - complete by EOD';
+    expect(getLatestNotifications()).toBe(expectedNotification)
+  });
+});
