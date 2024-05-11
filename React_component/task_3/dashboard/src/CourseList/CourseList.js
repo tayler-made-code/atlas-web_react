9b890b3064632fd33dde825d65a4f40e89b17834
F-O-P <tayler.coon@gmail.com>
2024-05-11 12:40:36 -0500

    task_3

---
+++ React_component/task_3/dashboard/src/CourseList/CourseList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CourseListRow from './CourseListRow';
+import CourseShape from './CourseShape';
+import './CourseList.css';
+
+const CourseList = ({ listCourses }) => {
+  return (
+    <table id='CourseList'>
+      <thead>
+        <CourseListRow isHeader={true} textFirstCell='Available courses' />
+        <CourseListRow isHeader={true} textFirstCell='Course name' textSecondCell='Credit' />
+      </thead>
+      <tbody>
+        {listCourses.length === 0 ? (
+          <CourseListRow isHeader={false} textFirstCell='No course available yet' />
+        ) : (
+          listCourses.map((course) => (
+            <CourseListRow
+              key={course.id}
+              textFirstCell={course.name}
+              textSecondCell={course.credit}
+            />
+          ))
+        )}
+      </tbody>
+    </table>
+  );
+};
+
+CourseList.propTypes = {
+  listCourses: PropTypes.arrayOf(CourseShape),
+}
+
+CourseList.defaultProps = {
+  listCourses: [],
+}
+
+export default CourseList;
---
+++ React_component/task_3/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function NotificationItem({ type, value, html, markAsRead, id }) {
+  const listItemContent = html ? (
+    <li
+      data-notification-type={type}
+      dangerouslySetInnerHTML={html}
+      onClick={() => markAsRead(id)}
+    />
+  ) : (
+    <li data-notification-type={type} onClick={() => markAsRead(id)}>
+      {value}
+    </li>
+  );
+
+  return listItemContent;
+};
+
+NotificationItem.propTypes = {
+  type: PropTypes.string,
+  value: PropTypes.string,
+  html: PropTypes.shape({
+    __html: PropTypes.string,
+  }),
+  markAsRead: PropTypes.func,
+  id: PropTypes.number,
+};
+
+NotificationItem.defaultProps = {
+  type: 'default',
+  markAsRead: () => {},
+};
+
+export default NotificationItem;
