76d4302dc531e89546655c77712ec92404f67c07
F-O-P <tayler.coon@gmail.com>
2024-05-19 18:39:52 -0500

    Login Tests all working

--- react_state/task_1/dashboard/src/Login/Login.test.js
+++ react_state/task_1/dashboard/src/Login/Login.test.js
@@ -15,4 +15,29 @@ describe ('Login Component', () => {
     expect(wrapper.find('input')).toHaveLength(3);
     expect(wrapper.find('label')).toHaveLength(2);
   });
+
+  it('submit button is disabled by default', () => {
+    const wrapper = shallow(<Login />);
+    const submitButton = wrapper.find('input[type="submit"]');
+    expect(submitButton.prop('disabled')).toBe(true);
+  });
+
+  it('submit button is enabled after email and password are filled in', () => {
+    const wrapper = shallow(<Login />);
+
+    const submitButtonBefore = wrapper.find('input[type="submit"]');
+    expect(submitButtonBefore.prop('disabled')).toBe(true);
+
+    const emailInput = wrapper.find('input[type="email"]');
+    emailInput.simulate('change', { target: { value: 'obiwan@jedi.com' } });
+
+    const submitButtonDuring = wrapper.find('input[type="submit"]');
+    expect(submitButtonDuring.prop('disabled')).toBe(true);
+
+    const passwordInput = wrapper.find('input[type="password"]');
+    passwordInput.simulate('change', { target: { value: 'kenobi123' } });
+
+    const submitButtonAfter = wrapper.find('input[type="submit"]');
+    expect(submitButtonAfter.prop('disabled')).toBe(false);
+  });
 });
