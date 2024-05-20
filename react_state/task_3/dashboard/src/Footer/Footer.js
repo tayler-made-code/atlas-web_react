25d2bbad376fc20a4cf5b69c6da9321a6f6f2ca4
F-O-P <tayler.coon@gmail.com>
2024-05-20 15:47:42 -0500

    footer updated

--- react_state/task_3/dashboard/src/Footer/Footer.js
+++ react_state/task_3/dashboard/src/Footer/Footer.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Footer.css';
 import { getFullYear, getFooterCopy } from '../utils/utils';
+import { AppContext } from '../App/AppContext';
 
 function Footer() {
+  const { user } = useContext(AppContext);
+
   return (
     <footer className="Footer">
       <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      {user.isLoggedIn && <p><a href="#">Contact us</a></p>}
     </footer>
   );
 }
--- react_state/task_3/dashboard/src/Footer/Footer.test.js
+++ react_state/task_3/dashboard/src/Footer/Footer.test.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Footer from './Footer';
+import { AppContext } from '../App/AppContext';
 
 describe('Footer Component', () => {
   it('renders without crashing', () => {
-    shallow(<Footer />);
+    shallow(<AppContext.Provider value={{ user: { isLoggedIn: false } }}><Footer /></AppContext.Provider>);
   });
 
   it('renders the text "Copyright"', () => {
     const wrapper = shallow(<Footer />);
     expect(wrapper.text()).toContain('Copyright');
+  });
+
+  it('does not display the link when the user is logged out', () => {
+    const wrapper = shallow(<AppContext.Provider value={{ user: { isLoggedIn: false } }}><Footer /></AppContext.Provider>);
+    expect(wrapper.text().includes('Contact us')).toBe(false);
+  });
+
+  it('displays the link when the user is logged in', () => {
+    const loggedInUser = { email: 'user@email.com', isLoggedIn: true };
+    const wrapper = mount(<AppContext.Provider value={{ user: loggedInUser }}><Footer /></AppContext.Provider>);
+    expect(wrapper.text().includes('Contact us')).toBe(true);
   });
 });
