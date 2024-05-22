35cef3a2e9e70c0d961e160f8ca6cebc5c44e789
F-O-P <tayler.coon@gmail.com>
2024-05-21 19:15:43 -0500

    task_1 working with tests

--- 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,4 +1,20 @@
 import * as notificationsData from '../../../../notifications.json'
+import { normalize, schema } from 'normalizr';
+
+const user = new schema.Entity('users');
+
+const message = new schema.Entity('messages', {}, {
+  idAttribute: 'guid',
+});
+
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message,
+}, {
+  idAttribute: 'id',
+});
+
+export const normalizedData = normalize(notificationsData.default, [notification]);
 
 export function getAllNotificationsByUser(userID) {
   return notificationsData.default
