cf44d5c72fb3b812b8fd120d09b3e9fd1d84f18b
F-O-P <tayler.coon@gmail.com>
2024-06-02 09:18:19 -0500

    setup for task_3

---
+++ react_redux_reducer_selector/task_3/dashboard/src/actions/courseActionCreators.js
@@ -0,0 +1,20 @@
+import { bindActionCreators } from "redux";
+import { SELECT_COURSE, UNSELECT_COURSE } from "./courseActionTypes";
+
+export const selectCourse = (index) => ({
+  type: SELECT_COURSE,
+  index,
+});
+
+export const unSelectCourse = (index) => ({
+  type: UNSELECT_COURSE,
+  index,
+});
+
+export const boundCourseActions = (dispatch) => bindActionCreators(
+  {
+    selectCourse,
+    unSelectCourse,
+  },
+  dispatch
+);
